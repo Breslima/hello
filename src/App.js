c5f4f68f950218cd0e175f1059722d44ef1d345e
Breslima <michael.j.breslin@gmail.com>
2023-09-15 19:21:37 +0100

    Next up Styling

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import "./App.css";
 import Employee from "./Components/Employee";
+import { useState } from "react";
 
 function App() {
+  const [role, setRole] = useState("dev");
+
   const showEmployees = true;
   return (
     <div className="App">
       {showEmployees ? (
         <>
+          <input
+            type="text"
+            onChange={(e) => {
+              console.log(e.target.value);
+              setRole(e.target.value);
+            }}
+          />
           <Employee name="Michael" role="intern" />
-          <Employee name="Abby" />
+          <Employee name="Abby" role={role} />
           <Employee name="John" />
         </>
       ) : (
